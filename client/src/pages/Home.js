5bd804d9ffae33ba9b079bbc4ee89e0997ec0635
Anand Chaudhary <anand92490@gmail.com>
2019-11-21 01:25:34 -0600

    clearing the console

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import { Col, Row, Container } from "../components/Grid";
-import Jumbotron from "../components/Jumbotron";
 import Header from '../component/Layout/Header';
 import Carousal from "../components/Carousel"
 
