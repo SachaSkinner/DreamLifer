92ced1eb665f541d2328f6dbe092442ac6921db8
BrianKohnen <briankohnen@yahoo.com>
2019-11-22 18:39:00 -0600

    bk--updated style on review display to wrap large text

--- client/src/components/ReviewStore/index.js
+++ client/src/components/ReviewStore/index.js
@@ -49,7 +49,7 @@ class ReviewStore extends Component {
         return (
             <div >
                 <h3>My memories </h3>
-                <div>
+                <div style={{overflowWrap: 'break-word'}}>
                     {this.state.allReviews.length !== 0 ?
                         this.state.allReviews.map(element => (
                             <p key={element._id}> {element.date}  <span style={floatRight} className='remove' dataid={element._id} onClick={this.handleRemove}>X</span><br></br> <br></br><strong>Family:</strong> {element.family} <br></br> <strong>Friends:</strong> {element.friends} <br></br> <strong>Work: </strong>{element.work}<br></br> <strong>Study:  </strong>{element.study}<br></br> <strong>Fun: </strong> {element.fun}<br></br> <strong>Food: </strong> {element.food}<br></br> <strong>Sleep: </strong> {element.sleep} <br></br> <strong>Mood:</strong> {element.mood}<br></br> <strong>Sport:</strong> {element.sport}<br></br><strong>Ideas:</strong>  {element.ideas} <br></br> <strong>Notes:</strong> {element.notes}<br></br> <strong> I am thankful for:  </strong>{element.thanks}<hr></hr></p>
