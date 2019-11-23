b9c63effe90f993f321833008e7038e911243e44
BrianKohnen <briankohnen@yahoo.com>
2019-11-22 18:53:34 -0600

    bk--added restrictions to user first name length, password. added restriction to setting a goal in the past"

--- client/src/components/ToDoSubmit/index.js
+++ client/src/components/ToDoSubmit/index.js
@@ -2,9 +2,12 @@ import React, { Component } from "react";
 // import { Link } from 'react-router-dom';
 import API from '../../utils/API';
 import './style.css';
+import moment from 'moment';
+moment().format();
 
 class Todo extends Component {
     state = {
+        now: '',
         message: '',
         headerMessage: 'Select a date, set your goals'
     };
@@ -35,10 +38,21 @@ class Todo extends Component {
         }
     };
 
+    getNowDate = () => {
+        let Mnow = moment(new Date()).format('YYYY-MM-DD');
+        this.setState({ now: Mnow });
+    };
+
+    componentDidMount = () => {
+        this.getNowDate();
+    };
+
     render() {
 
         return (
         <div className='signupWrapper'>
+         {this.state.now === moment(this.props.calendarDate).format('YYYY-MM-DD') ? (
+            <>
             <h3>
                 <p>{this.props.calendarDate}</p>
                 {this.state.headerMessage}
@@ -47,6 +61,10 @@ class Todo extends Component {
                 <input className="todoInput" value={this.state.message} name='message' onChange={this.handleInputChange} placeholder='What do you need to accomplish?'></input>
                 <button onClick={this.handleSubmit} className='btn btn-outline-dark submitTodo'>submit</button>
             </form>
+            </> ) : 
+            (
+                <h3>You can't set a goal for the past! Sorry!</h3>
+            )}
         </div>
         );
     };
